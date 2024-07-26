ddf473f19514fd77f8585e13aa711badb3d1cbc2
Mateo <brandonahumada804@gmail.com>
2024-07-26 17:04:48 -0300

    scroll y home prueba

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
-import MobileNavbar from './assets/components/navbar/Navbar';
+import MobileNavbar from './components/navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './view/Home/Home';
+
 
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
     <MobileNavbar/> 
       <Routes>
         <Route path="/" element={<div>Home Page</div>} />
+        <Route path='/home' element={<Home/>}/>
       </Routes>
     </BrowserRouter>
   );
